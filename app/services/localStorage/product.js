e210fea098c4afc54a9e27d56b134180a42cee94
Andrius Radzivonas <32072309+niqqq25@users.noreply.github.com>
2019-12-03 21:09:45 +0200

    feat: #6 add localStorage (#13)
    * add localStorage
    
    * fix requested changes

---
+++ app/services/localStorage/product.js
@@ -0,0 +1,49 @@
+import { AsyncStorage } from 'react-native';
+import nanoid from 'nanoid/non-secure';
+
+async function get(productId) {
+    const products = await getAll();
+    return products?.find(product => product._id === productId);
+}
+
+async function getAll() {
+    try {
+        const products = (await AsyncStorage.getItem('products')) || [];
+        return JSON.parse(products);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function remove(productId) {
+    try {
+        const products = (await getAll()) || [];
+        await AsyncStorage.setItem(
+            'products',
+            JSON.stringify(products.filter(product => product._id !== productId))
+        );
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function removeAll() {
+    try {
+        await AsyncStorage.removeItem('products');
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function set(product) {
+    try {
+        const products = (await getAll()) || [];
+        product._id = nanoid();
+
+        await AsyncStorage.setItem('products', JSON.stringify([...products, product]));
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export default { get, getAll, remove, set, removeAll };
