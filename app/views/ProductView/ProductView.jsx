700032cf34d67690e778afda1dc7ec97646c0490
Julius Lungys <32368314+voidpumpkin@users.noreply.github.com>
2019-12-05 20:24:40 +0200

    feat: #25 remove product (#37)
    * feat: #25 remove product
    
    * add modal

--- app/views/ProductView/ProductView.jsx
+++ app/views/ProductView/ProductView.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Alert } from 'react-native';
+import { IconButton, Colors } from 'react-native-paper';
 import productStorage from '../../services/localStorage/product';
+import { ROUTE_NAMES } from '../../constants';
 
 const ProductView = ({ navigation }) => {
     const id = navigation.getParam('id', '');
@@ -16,6 +18,32 @@ const ProductView = ({ navigation }) => {
     }
 
     const { title, amount } = product;
+
+    const remove = async () => {
+        await productStorage.remove(id);
+        navigation.navigate(ROUTE_NAMES.HOME);
+    };
+
+    const handleTrash = async () => {
+        Alert.alert(
+            'Delete product',
+            `Are you sure you want to delete ${title} ?`,
+            [
+                {
+                    text: 'Cancel',
+                    onPress: () => {},
+                    style: 'cancel'
+                },
+                {
+                    text: 'Delete',
+                    onPress: remove,
+                    style: 'destructive'
+                }
+            ],
+            { cancelable: false }
+        );
+    };
+
     const expiryDate = new Date(product.expiryDate);
     const day = expiryDate.getDate();
     const month = expiryDate.getMonth() + 1;
@@ -23,9 +51,17 @@ const ProductView = ({ navigation }) => {
 
     return (
         <View style={styles.product}>
-            <Text id="title" style={styles.title}>
-                {title}
-            </Text>
+            <View style={styles.topRow}>
+                <Text id="title" style={styles.title}>
+                    {title}
+                </Text>
+                <IconButton
+                    icon="trash-can"
+                    color={Colors.red500}
+                    size={30}
+                    onPress={handleTrash}
+                />
+            </View>
             <Text style={styles.label}>{'Galioja iki'}</Text>
             <Text style={styles.detail}>{`${year}-${month}-${day}`}</Text>
             <Text style={styles.label}>{`Kiekis`}</Text>
@@ -49,6 +85,11 @@ const styles = StyleSheet.create({
     detail: {
         fontSize: 16,
         marginBottom: 5
+    },
+    topRow: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
     }
 });
 
